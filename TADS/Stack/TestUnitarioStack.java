d07c9cfe3325ab260b1ec97ab28fd5a08f31d4d3
Paula <psteverlynck@correo.um.edu.uy>
2024-05-15 16:58:48 -0300

    NUEVOS

--- TADS/Stack/TestUnitarioStack.java
+++ TADS/Stack/TestUnitarioStack.java
@@ -1,13 +1,44 @@
 package TADS.Stack;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import TADS.LinkedList.src.LinkedList;
+import TADS.LinkedList.src.MyList;
+
 
 
     public class TestUnitarioStack <T extends Comparable<T>>{
-
         @Test
-        public void pop(){
+        public void validar() throws EmptyStackException {
+            MyStack<Integer> mistack=new LinkedStack();
+            mistack.push(1);
+            mistack.push(2);
+            mistack.push(3);
+            MyList<Integer> resultado=new LinkedList();
+            while (!mistack.isEmpty()){
+                resultado.add(mistack.top());
+                mistack.pop();}
+            MyList<Integer> expected=new LinkedList();
+            expected.add(3);
+            expected.add(2);
+            expected.add(1);
+            for(int i=0; i<=expected.size();i++) {
+                assertEquals(expected.get(i), resultado.get(i));
+            }
 
         }
+        @Test
+        public void makeempty(){
+            MyStack<Integer> mistack=new LinkedStack();
+            mistack.push(1);
+            mistack.push(2);
+            mistack.push(3);
+            mistack.makeEmpty();
+            int i=0;
+            while (!mistack.isEmpty()){
+                i++;
+            }
+            assertEquals(i,0);
 
+
+        }
 }
--- TADS/Tree/TestUnitarioTree.java
+++ TADS/Tree/TestUnitarioTree.java
@@ -2,11 +2,10 @@ package TADS.Tree;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import TADS.LinkedList.src.LinkedList;
 import TADS.LinkedList.src.MyList;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
     public class TestUnitarioTree <K extends Comparable<K>,T>{
         @Test
@@ -19,22 +18,30 @@ import java.util.List;
             miarbolbi.insert(5, "E");
             miarbolbi.insert(1, "F");
             miarbolbi.insert(7, "G");
-            MyList<Integer> resultado1 = (MyList<Integer>) miarbolbi.inOrder();
-            MyList<Integer> expected1 = (MyList<Integer>) Arrays.asList(1, 2, 5, 7, 8, 9);
-            assertEquals(expected1.size(), resultado1.size());
-            assertEquals(expected1, resultado1);
+            MyList<Integer> resultado = miarbolbi.inOrder();
+            MyList<Integer> expected = new LinkedList<>();
+            expected.add(1);
+            expected.add(2);
+            expected.add(5);
+            expected.add(7);
+            expected.add(8);
+            expected.add(9);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
         }
         @Test
         public void findbinary() {
             TreeNode<Integer, String> raiz = new TreeNode<>(5, "A", null, null);
-            MyBinarySearchTree miarbolbi2 = new BinarySearchTree(raiz);
-            miarbolbi2.insert(2, "B");
-            miarbolbi2.insert(8, "C");
-            miarbolbi2.insert(9, "D");
-            miarbolbi2.insert(5, "E");
-            miarbolbi2.insert(1, "F");
-            miarbolbi2.insert(7, "G");
-            String resultado= (String) miarbolbi2.findbus(2);
+            MyBinarySearchTree miarbolbi = new BinarySearchTree(raiz);
+            miarbolbi.insert(2, "B");
+            miarbolbi.insert(8, "C");
+            miarbolbi.insert(9, "D");
+            miarbolbi.insert(5, "E");
+            miarbolbi.insert(1, "F");
+            miarbolbi.insert(7, "G");
+            String resultado= (String) miarbolbi.findbus(2);
             String expected="B";
             assertEquals(expected, resultado);
         }
@@ -42,67 +49,100 @@ import java.util.List;
         @Test
         public void deleteraizbinary(){
             TreeNode<Integer, String> raiz = new TreeNode<>(5, "A", null, null);
-            MyBinarySearchTree miarbolbi3 = new BinarySearchTree(raiz);
-            miarbolbi3.insert(2, "B");
-            miarbolbi3.insert(8, "C");
-            miarbolbi3.insert(9, "D");
-            miarbolbi3.insert(5, "E");
-            miarbolbi3.insert(1, "F");
-            miarbolbi3.insert(7, "G");
-            miarbolbi3.delete(5);
-            MyList<Integer> resultado3 = (MyList<Integer>) miarbolbi3.inOrder();
-            MyList<Integer> expected3 = (MyList<Integer>) Arrays.asList(2,1,7,8,9);
-            assertEquals(expected3.size(), resultado3.size());
-            assertEquals(expected3, resultado3);
+            MyBinarySearchTree miarbolbi = new BinarySearchTree(raiz);
+            miarbolbi.insert(2, "B");
+            miarbolbi.insert(8, "C");
+            miarbolbi.insert(9, "D");
+            miarbolbi.insert(5, "E");
+            miarbolbi.insert(1, "F");
+            miarbolbi.insert(7, "G");
+            miarbolbi.delete(5);
+            MyList<Integer> resultado = miarbolbi.inOrder();
+            MyList<Integer> expected = new LinkedList<>();
+            expected.add(2);
+            expected.add(1);
+            expected.add(7);
+            expected.add(8);
+            expected.add(9);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
+
 
         }
 
         @Test
         public void deleteUnHijobinary(){
             TreeNode<Integer, String> raiz = new TreeNode<>(5, "A", null, null);
-            MyBinarySearchTree miarbolbi3 = new BinarySearchTree(raiz);
-            miarbolbi3.insert(2, "B");
-            miarbolbi3.insert(8, "C");
-            miarbolbi3.insert(9, "D");
-            miarbolbi3.insert(5, "E");
-            miarbolbi3.insert(1, "F");
-            miarbolbi3.insert(7, "G");
-            miarbolbi3.delete(2);
-            MyList<Integer> resultado3 = (MyList<Integer>) miarbolbi3.inOrder();
-            MyList<Integer> expected3 = (MyList<Integer>) Arrays.asList(1,5,7,8,9);
-            assertEquals(expected3, resultado3);
+            MyBinarySearchTree miarbolbi = new BinarySearchTree(raiz);
+            miarbolbi.insert(2, "B");
+            miarbolbi.insert(8, "C");
+            miarbolbi.insert(9, "D");
+            miarbolbi.insert(5, "E");
+            miarbolbi.insert(1, "F");
+            miarbolbi.insert(7, "G");
+            miarbolbi.delete(2);
+            MyList<Integer> resultado = miarbolbi.inOrder();
+            MyList<Integer> expected = new LinkedList<>();
+            expected.add(1);
+            expected.add(5);
+            expected.add(7);
+            expected.add(8);
+            expected.add(9);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
         }
 
         @Test
         public void deleteDosHijosbinary(){
             TreeNode<Integer, String> raiz = new TreeNode<>(5, "A", null, null);
-            MyBinarySearchTree miarbolbi3 = new BinarySearchTree(raiz);
-            miarbolbi3.insert(2, "B");
-            miarbolbi3.insert(8, "C");
-            miarbolbi3.insert(9, "D");
-            miarbolbi3.insert(5, "E");
-            miarbolbi3.insert(1, "F");
-            miarbolbi3.insert(7, "G");
-            miarbolbi3.delete(8);
-            MyList<Integer> resultado3 = (MyList<Integer>) miarbolbi3.inOrder();
-            MyList<Integer> expected3 = (MyList<Integer>) Arrays.asList(1,2,5,9,7);
-            assertEquals(expected3, resultado3);
+            MyBinarySearchTree miarbolbi = new BinarySearchTree(raiz);
+            miarbolbi.insert(2, "B");
+            miarbolbi.insert(8, "C");
+            miarbolbi.insert(9, "D");
+            miarbolbi.insert(5, "E");
+            miarbolbi.insert(1, "F");
+            miarbolbi.insert(7, "G");
+            miarbolbi.delete(8);
+            MyList<Integer> resultado = miarbolbi.inOrder();
+            MyList<Integer> expected = new LinkedList<>();
+            expected.add(1);
+            expected.add(2);
+            expected.add(5);
+            expected.add(9);
+            expected.add(7);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
         }
 
         @Test
         public void deleteNoExistebinary(){
             TreeNode<Integer, String> raiz = new TreeNode<>(5, "A", null, null);
-            MyBinarySearchTree miarbolbi3 = new BinarySearchTree(raiz);
-            miarbolbi3.insert(2, "B");
-            miarbolbi3.insert(8, "C");
-            miarbolbi3.insert(9, "D");
-            miarbolbi3.insert(5, "E");
-            miarbolbi3.insert(1, "F");
-            miarbolbi3.insert(7, "G");
-            miarbolbi3.delete(4);
-            MyList<Integer> resultado3 = (MyList<Integer>) miarbolbi3.inOrder();
-            MyList<Integer> expected3 = (MyList<Integer>) Arrays.asList(1,2,5,7,8,9);
-            assertEquals(expected3, resultado3);
+            MyBinarySearchTree miarbolbi = new BinarySearchTree(raiz);
+            miarbolbi.insert(2, "B");
+            miarbolbi.insert(8, "C");
+            miarbolbi.insert(9, "D");
+            miarbolbi.insert(5, "E");
+            miarbolbi.insert(1, "F");
+            miarbolbi.insert(7, "G");
+            miarbolbi.delete(4);
+            MyList<Integer> resultado = miarbolbi.inOrder();
+            MyList<Integer> expected = new LinkedList<>();
+            expected.add(1);
+            expected.add(2);
+            expected.add(5);
+            expected.add(7);
+            expected.add(8);
+            expected.add(9);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
         }
 
         @Test
@@ -113,9 +153,15 @@ import java.util.List;
             miarbol.insert(3,"C",1);
             miarbol.insert(3,"Z",2);
             miarbol.insert(8,"Z",1);
-            MyList<Integer> result = (MyList<Integer>) miarbol.inOrder();
-            MyList<Integer> expected= (MyList<Integer>) Arrays.asList(2,1,3);
-            assertEquals(expected, result);
+            MyList<Integer> resultado =  miarbol.inOrder();
+            MyList<Integer> expected= new LinkedList<>();
+            expected.add(2);
+            expected.add(1);
+            expected.add(3);
+            assertEquals(expected.size(), resultado.size());
+            for(int i=0;i<=expected.size();i++){
+                assertEquals(expected.get(i), resultado.get(i));
+            }
         }
 
         @Test
@@ -142,13 +188,18 @@ import java.util.List;
             miarbol.insert(5,"E",2);
             miarbol.insert(6,"F",4);
             miarbol.delete(3);
-            MyList<Integer> result = (MyList<Integer>) miarbol.inOrder();
-            MyList<Integer> expected= (MyList<Integer>) Arrays.asList(6,4,2,5,1);
+            MyList<Integer> result =  miarbol.inOrder();
+            MyList<Integer> expected= new LinkedList<>();
+            expected.add(6);
+            expected.add(4);
+            expected.add(2);
+            expected.add(5);
+            expected.add(1);
             assertEquals(expected, result);
             miarbol.delete(2);
-            MyList<Integer> result1 = (MyList<Integer>) miarbol.inOrder();
-            MyList<Integer> expected1= (MyList<Integer>) Arrays.asList(6,4,5,1);
-            assertEquals(expected1, result1);
+            expected.remove(2);
+            result=miarbol.inOrder();
+            assertEquals(expected, result);
         }
 
         @Test
@@ -161,11 +212,4 @@ import java.util.List;
             Integer expected=3;
             assertEquals(resultado,expected);
         }
-
-
-
-
-
-
-
     }
