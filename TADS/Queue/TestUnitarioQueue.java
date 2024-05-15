bfd3d8b90de221a319f5686949868cc36c451437
tassa <ftassani@correo.um.edu.uy>
2024-05-15 16:55:13 -0300

    fede

--- TADS/Queue/TestUnitarioQueue.java
+++ TADS/Queue/TestUnitarioQueue.java
@@ -1,29 +1,34 @@
 package TADS.Queue;
 
-import TADS.LinkedList.src.LinkedList;
-import TADS.LinkedList.src.MyList;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
 
 public class TestUnitarioQueue {
     @Test
-    public void validarenqueue(){
+    public void validarQueueEnqueuDequeu(){
         MyQueue<Integer> myQueue= new LinkedQueue<Integer>();
         myQueue.enqueue(1);
         myQueue.enqueue(2);
         myQueue.enqueue(3);
+        try {
+            assertEquals(1, myQueue.dequeue());
+            assertEquals(2, myQueue.dequeue());
+            assertEquals(3, myQueue.dequeue());
+            assertEquals("la cola esta vacia", myQueue.dequeue());
+        } catch (EmptyQueueException e){;
+        }
+    }
+    @Test
+    public void validarIsEmpty(){
+        MyQueue<Integer> myQueue= new LinkedQueue<Integer>();
+        assertEquals(true,myQueue.isEmpty());
+    }
+    @Test
+    public  void validarIsNotEmpty(){
+        MyQueue<Integer> myQueue= new LinkedQueue<Integer>();
+        myQueue.enqueue(1);
+        assertEquals(false,myQueue.isEmpty());
     }
 
-    public boolean validar(MyQueue<Integer> resultado, MyList<Integer> expected) {
-        MyList <Integer> listaDeLaCola = new LinkedList<>();
-        while (!resultado.isEmpty()) {
-            try {
-                listaDeLaCola.add(resultado.dequeue());
-            } catch (EmptyQueueException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return true;
-    }
 }
