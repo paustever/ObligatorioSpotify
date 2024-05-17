5d93e37cbad0c0ffb296d615825bef17c173df54
Paula <psteverlynck@correo.um.edu.uy>
2024-05-17 14:28:11 -0300

    NUEVOS

---
+++ TADS/Hash/Hash.java
@@ -0,0 +1,18 @@
+package TADS.Hash;
+
+public class Hash implements MyHash{
+    @Override
+    public boolean pertenece() {
+        return false;
+    }
+
+    @Override
+    public boolean insert() {
+        return false;
+    }
+
+    @Override
+    public boolean eliminar() {
+        return false;
+    }
+}
---
+++ TADS/Hash/MyHash.java
@@ -0,0 +1,7 @@
+package TADS.Hash;
+
+public interface MyHash {
+    boolean pertenece(object);
+    boolean insert (object);
+    boolean eliminar (object);
+}
